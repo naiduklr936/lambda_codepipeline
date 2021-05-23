1be2432eca0a3f4496ae60728fc34684f9270413
Naidu Kandulapati <naiduklr936@gmail.com>
2021-05-23 15:37:37 -0500

    testing lambda changes

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -4,5 +4,5 @@ import json
 ec2=boto3.client('ec2')
 
 def lambda_handler(event, context):
-    res = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(res)}
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(response)}

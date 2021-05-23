b3ac2f5ca47713fe56b95d6cbb666eb6718aaa0d
Naidu Kandulapati <naiduklr936@gmail.com>
2021-05-23 12:48:47 -0500

    updating the response variable

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -4,6 +4,5 @@ import json
 ec2=boto3.client('ec2')
 
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
-    
+    res = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(res)}
